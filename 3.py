0eae67af151d61725b6382cb8e6b80b2159ce2d4
Humayun Khan <humayunk.pvt@gmail.com>
2023-02-25 18:52:35 +0530

    init

---
+++ 3.py
@@ -0,0 +1,47 @@
+# -------- Write a program to convert U.S. dollars to Indian rupees --------
+usd = 45
+inr = usd * 82.9
+print("The currency in INR is", round(inr, 2)); print("\n")
+# -------- Write a program to convert bits to Megabytes, Gigabytes and Terabytes --------
+b = 1000000
+B = b/8
+KB = B/1024
+MB = KB/1024
+GB = MB/1024
+TB = GB/1024
+print("MB:", MB)
+print("GB:", GB)
+print("TB:", TB); print("\n")
+# -------- Write a program to find the square root of a number --------
+Square = 8
+SuqareRoot = Square ** 0.5
+print("The Square Root is:", Square); print("\n")
+# -------- Write a program to find the area of Rectangle --------
+width = 30
+height = 15
+Area = width*height
+print("The Area of a Reactangle is", Area); print("\n")
+# -------- Write a program to calculate area and perimeter of the square --------
+Side = 18
+SArea = Side*Side
+print("The Area of Square is", SArea)
+Perimeter = 4*Side
+print("The Perimeter of Square is", Perimeter); print("\n")
+# -------- Write a program to calculate surface volume and area of a cylinder --------
+pi = 3.14
+height = 30
+radius = 20
+volume = pi * radius * radius * height
+print("The Surface Volume of cylinder is", volume)
+CArea = ((2*pi*radius) * height) + ((pi*radius**2) * 2)
+print("The Area of Cylinder is", CArea); print("\n")
+# -------- Write a program to swap the value of two variables --------
+x = 5
+y = 10
+print("value of x:", x)
+print("value of y:", y)
+temp = x
+x = y
+y = temp
+print("The value of x after swapping:", x)
+print("The value of y after swapping:", y); print("\n")
